cbc9639289e4a84ee0376f6d655a9ac16b749b30
Lucas Ribeiro <lucasribeiro20@hotmail.com>
2024-05-24 10:30:22 -0300

    teste consumo api

--- imdb/src/main/java/br/com/lctt/imdb/ImdbApplication.java
+++ imdb/src/main/java/br/com/lctt/imdb/ImdbApplication.java
@@ -2,12 +2,30 @@ package br.com.lctt.imdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ImdbApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ImdbApplication.class, args);
+
+		testaChamada();
+
+
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	private static void testaChamada() {
+		RestTemplate restTemplate =new RestTemplate();
+		Object obj = restTemplate.getForObject("https://api.themoviedb.org/3/movie/11?api_key=myApiKey", Object.class);
+		System.out.println(obj.toString());
 	}
 
 }
